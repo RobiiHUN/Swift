dd0f9846fb90060d4db2cb5627e3ff58d28001c7
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2024-06-02 14:56:58 +0200

    06.02

---
+++ elso_app/elso_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  elso_app
+//
+//  Created by Ötvös Róbert on 02/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ elso_app/elso_app/elso_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  elso_appApp.swift
+//  elso_app
+//
+//  Created by Ötvös Róbert on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct elso_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
