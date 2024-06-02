c0137ec635f470adb6cb34942fffe423edf50e0a
RobiiiHUN <otrobesz10@gmail.com>
2024-06-02 20:32:20 +0200

    text aligments

---
+++ elso_app/elso_app/Text.swift
@@ -0,0 +1,18 @@
+//
+//  Text.swift
+//  elso_app
+//
+//  Created by Ötvös Róbert on 02/06/2024.
+//
+
+import SwiftUI
+
+struct Text: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Text()
+}
