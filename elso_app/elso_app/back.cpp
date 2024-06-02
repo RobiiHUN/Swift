c789a26522fb5dcdb4c8053cbd3186ed14effdd7
RobiiiHUN <otrobesz10@gmail.com>
2024-06-02 15:56:28 +0200

    06.02

---
+++ elso_app/elso_app/back.cpp
@@ -0,0 +1,8 @@
+//
+//  back.cpp
+//  elso_app
+//
+//  Created by Ötvös Róbert on 02/06/2024.
+//
+
+#include "back.hpp"
---
+++ playground/playground.swiftpm/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  playground
+//
+//  Created by Ötvös Róbert on 02/06/2024.
+//
+
+import Foundation
