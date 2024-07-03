dd5c25bf363869c4afa7f82386eec26f9d67e4bb
RobiiiHUN <otrobesz10@gmail.com>
2024-07-03 11:40:46 +0200

    07.03

--- elso_app/elso_app/Text.swift
+++ elso_app/elso_app/Text.swift
@@ -1,5 +1,5 @@
 //
-//  Text.swift
+//  MyText.swift
 //  elso_app
 //
 //  Created by Ötvös Róbert on 02/06/2024.
@@ -7,12 +7,23 @@
 
 import SwiftUI
 
-struct Text: View {
+struct MyText: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Mi a baj?")
+        Text("Mi újság? Hogy megy a fityeg? ehriohfeohfdshelsdjflsdhfhsdlhfkhlks")
+           // .font(.body)
+           // .fontWeight(.medium)
+            //.font(.system(size: 24, weight: .semibold, design: .serif))
+            //.baselineOffset(20)    //terkoz
+            //.kerning(1) //karakterkozok
+            //.multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 300, height: 100, alignment: .leading)
+            .minimumScaleFactor(0.1)        //hanyad reszere tud csokkeni a tartalom
+            
     }
 }
 
 #Preview {
-    Text()
+    MyText()
 }
